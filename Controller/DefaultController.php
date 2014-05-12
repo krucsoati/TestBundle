25e6428af0a1dda5de2325bb6f592a8d07d7c46a
krucso <krucso.attila@tmx.hu>
2014-05-12 16:45:22 +0200

    init commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace krucsoati\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('krucsoatiTestBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ krucsoatiTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace krucsoati\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class krucsoatiTestBundle extends Bundle
+{
+}
